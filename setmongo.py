f6329c4dfe8d5c720f48be8f49add74f8a0a7557
Your Name <zheng.wang@netbraintech.com>
2016-03-29 20:22:40 +0800

    modify class init function

--- setmongo.py
+++ setmongo.py
@@ -1,28 +1,49 @@
 from pymongo import MongoClient
 class GetMonogdb():
-    def __init__(self,ip,port):
+
+    def __init__(
+            self,
+            ip,
+            port,
+            db,
+            collect):
         self.ip=ip
         self.port=port
-    def get_dbs(self,db):
-        client= MongoClient(self.ip,self.port)
-        dbs=client[db]
-        return dbs
+        self.db=db
+        self.collect=collect
 
-    def get_collect(self,db,collect):
-        dbs=self.get_dbs(db)
-        bss=dbs[collect]
-        return bss
-    def find_all(self,dbss,collects,condition=None):
+    def _conn_db(self):
+        client= MongoClient(self.ip,self.port)
+        return client[self.db]
+    def _conn_collect(self):
+        dbs=self._conn_db()
+        collects=dbs[self.collect]
+        return  collects
+    def find_all(self,condition=None):
         data=[]
-        collect=self.get_collect(dbss,collects)
-        for i in collect.find(condition):
+        datas=self._conn_collect().find(condition)
+        for i in datas:
             yield i
             data.append(i)
+    def insert_all(self,data):
+        collect=self._conn_collect()
+        print type(collect)
+        collect.insert(data)
+
+    def find_limits(self,limits):
+        data=[]
+        datas=self._conn_collect().find_one(limit=limits)
+        for row in datas:
+            data.append(row)
+        return data
+
 
 
 if __name__ == '__main__':
-    moge=GetMonogdb("10.10.5.189",27017)
-    db=moge.find_all("CLI-mini_task","Device",{'mip':'10.10.10.16'})
-    for i in db:
-        print i["mip"]
-
+    moge=GetMonogdb("10.10.5.189",27017,"CLI-mini_task","Device")
+    print moge.find_limits(5)
+    #db=moge.find_all("CLI-mini_task","Device",{'mip':'10.10.10.16'})
+    # for i in db:
+    #     print i["mip"
+    # for i in moge.find_all():
+    #     print i
